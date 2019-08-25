1574b5f67b18a91938f2bf59cd0cd907f2b5f07e
The San Cao <caothesan@gmail.com>
2019-08-25 16:37:30 +0700

    Update new version

--- template.cpp
+++ template.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 #define REP(i, n) for (int i = 0; i < n; ++i)
 #define FOR(i, a, b) for (int i = a; i <= b; ++i)
 #define FORD(i, b, a) for (int i = b; i >= a; --i)
@@ -13,3 +11,4 @@ using namespace std;
 #define DEBUG(x) cerr << #x << " = " << x << endl;
 #define DEBUGS(v, a, b) cerr << #v << " = "; FOR(i, a, b) cerr << v[i] << " "; cerr << endl;
 
+using namespace std;
